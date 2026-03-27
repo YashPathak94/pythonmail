1b38da938a269bbfba90981804fb9b242ea08b9b
YashPathak94 <86586754+YashPathak94@users.noreply.github.com>
2026-03-27 19:09:42 +0530

    Update Pods.sh

--- Pods.sh
+++ Pods.sh
@@ -12,3 +12,23 @@ END {
     print "----------------------"
   }
 }'
+
+
+
+kubectl get pods -A -o json | jq -r '
+.items[] |
+"\(.metadata.labels.app) \(.spec.nodeName) \(.metadata.name)"' | sort | \
+awk '
+{
+  key=$1" "$2
+  pods[key]=pods[key]" "$3
+  count[key]++
+}
+END {
+  for (k in pods) {
+    if (count[k] > 1) {
+      print "Same app on same node:", k
+      print "Pods:", pods[k]
+      print "---------------------"
+    }
+}'
