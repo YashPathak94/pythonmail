f4c770f3e1557c27bf70a13865363335764b4645
YashPathak94 <86586754+YashPathak94@users.noreply.github.com>
2026-03-27 19:35:29 +0530

    Update Pods.sh

--- Pods.sh
+++ Pods.sh
@@ -32,3 +32,53 @@ END {
       print "---------------------"
     }
 }'
+
+kubectl get pods -A -o json | jq -r '
+.items[] |
+"\(.metadata.namespace)\t\(.metadata.labels.app // "NULL")\t\(.spec.nodeName)\t\(.metadata.name)"
+' | sort | awk '
+BEGIN {
+  GREEN="\033[32m";
+  YELLOW="\033[33m";
+  BLUE="\033[34m";
+  RESET="\033[0m";
+
+  printf "%-20s %-25s %-30s %-50s\n", "NAMESPACE", "APP", "NODE", "POD"
+  print "--------------------------------------------------------------------------------------------------------------------------"
+}
+{
+  ns=$1
+  app=$2
+  node=$3
+  pod=$4
+
+  key=app"|"node
+  count[key]++
+
+  data[NR]=$0
+}
+END {
+  for (i=1; i<=NR; i++) {
+    split(data[i], f, "\t")
+    ns=f[1]; app=f[2]; node=f[3]; pod=f[4]
+    key=app"|"node
+
+    # App coloring
+    app_color=app
+    if (app=="NULL") {
+      app_color=GREEN app RESET
+    } else if (count[key] > 1) {
+      app_color=BLUE app RESET
+    }
+
+    # Pod coloring
+    pod_color=pod
+    if (pod ~ /proda/) {
+      pod_color=GREEN pod RESET
+    } else if (pod ~ /prodb/) {
+      pod_color=YELLOW pod RESET
+    }
+
+    printf "%-20s %-25s %-30s %-50s\n", ns, app_color, node, pod_color
+  }
+}'
