0c273bfb43b3e8dd256f9bf52afaa2b4d0344a70
YashPathak94 <86586754+YashPathak94@users.noreply.github.com>
2026-03-27 19:45:59 +0530

    Update Pods.sh

--- Pods.sh
+++ Pods.sh
@@ -82,3 +82,59 @@ END {
     printf "%-20s %-25s %-30s %-50s\n", ns, app_color, node, pod_color
   }
 }'
+
+
+
+kubectl get pods -A -o json | jq -r '
+.items[] |
+[
+  .metadata.namespace,
+  (.metadata.ownerReferences[0].name // "no-owner"),
+  .spec.nodeName,
+  .metadata.name
+] | @tsv
+' | sed -E 's/-[a-z0-9]{9,10}$//' | sort | awk '
+BEGIN {
+  RED="\033[31m";
+  GREEN="\033[32m";
+  YELLOW="\033[33m";
+  BLUE="\033[34m";
+  RESET="\033[0m";
+
+  printf "%-20s %-35s %-30s %-50s\n", "NAMESPACE", "DEPLOYMENT", "NODE", "POD"
+  print "----------------------------------------------------------------------------------------------------------------------------------"
+}
+{
+  ns=$1
+  deploy=$2
+  node=$3
+  pod=$4
+
+  key=deploy"|"node
+  count[key]++
+
+  data[NR]=$0
+}
+END {
+  for (i=1; i<=NR; i++) {
+    split(data[i], f, "\t")
+    ns=f[1]; deploy=f[2]; node=f[3]; pod=f[4]
+    key=deploy"|"node
+
+    # 🔴 Highlight same deployment pods on same node
+    deploy_color=deploy
+    if (count[key] > 1 && deploy != "no-owner") {
+      deploy_color=RED deploy RESET
+    }
+
+    # Pod color rules
+    pod_color=pod
+    if (pod ~ /proda/) {
+      pod_color=GREEN pod RESET
+    } else if (pod ~ /prodb/) {
+      pod_color=YELLOW pod RESET
+    }
+
+    printf "%-20s %-35s %-30s %-50s\n", ns, deploy_color, node, pod_color
+  }
+}'
